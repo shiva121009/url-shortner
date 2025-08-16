8514ea4f61989e901f192e5b8fa409d8df9934a6
Shivender Sharma <shivasharma931047@gmail.com>
2025-08-16 18:15:53 +0530

    last commit 3

--- app.js
+++ app.js
@@ -90,6 +90,8 @@ const server = createServer(async (req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
